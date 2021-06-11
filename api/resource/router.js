3a926ae60914dec1f3f475cfc00df012dbddf357
Daniel Walker <dw765608@gmail.com>
2021-06-11 11:36:11 -0400

    all routers and server connected

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,13 @@
 // build your `/api/resources` router here
+
+//server setup
+const express = require('express')
+const resources = express.Router()
+
+//test endpoint
+resources.get("/test",(req, res,next) => {
+    res.json({message:"connection successful"})
+})
+
+//export
+module.exports =resources
