328058d0b5e925ba58d85ac83b62944d46515e44
Daniel Walker <dw765608@gmail.com>
2021-06-11 16:23:25 -0400

    first midlleware up

--- api/task/model.js
+++ api/task/model.js
@@ -1,50 +1,50 @@
 // build your `Task` model here
-const db =require("../../data/dbConfig")
-const getAll=async()=>{
+const db = require("../../data/dbConfig")
+const getAll = async () => {
     const rows = db("projects as p")
-    .leftJoin("tasks as t","p.project_id","t.project_id")
-    .select("p.project_name","p.project_description","t.task_notes","task_description","task_completed")
+        .leftJoin("tasks as t", "p.project_id", "t.project_id")
+        .select("p.project_name", "p.project_description", "t.task_notes", "task_description", "task_completed")
 
     const data = await rows
-    const revisedData=data.map(item=>{
-        const {project_name,project_description,task_notes,task_description,task_completed}=item
-     //transforms data to boolean
-        const transform ={
-           "project_name":project_name,
-           "project_description":project_description,
-           "task_notes": task_notes,
-           "task_description": task_description,
-           "task_completed": task_completed===0?false:true
+    const revisedData = data.map(item => {
+        const { project_name, project_description, task_notes, task_description, task_completed } = item
+        //transforms data to boolean
+        const transform = {
+            "project_name": project_name,
+            "project_description": project_description,
+            "task_notes": task_notes,
+            "task_description": task_description,
+            "task_completed": task_completed === 0 ? false : true
         }
         return transform
-     })
+    })
     return revisedData
 }
-const findTaskbyId=task_id=>{
-   const rows= db("tasks")
-    .where("task_id",task_id)
+const findTaskbyId = task_id => {
+    const rows = db("tasks")
+        .where("task_id", task_id)
     return rows
 }
 
-const insert= async (newTask)=>{
+const insert = async (newTask) => {
     const rows = await db("tasks")
-    .insert(newTask)
-    .select("task_notes","task_description","task_completed")
+        .insert(newTask)
+        .select("task_notes", "task_description", "task_completed")
     const id = await rows[0]
     const data = await findTaskbyId(id)
-    const {task_notes,task_description,task_completed}=await data[0]
+    const { task_notes, task_description, task_completed } = await data[0]
     console.log(data)
     //transforms data to boolean
-    const transform =await {
-          "task_notes": task_notes,
-          "task_description": task_description,
-          "task_completed": task_completed===0?false:true
-       }
-  
-    
+    const transform = await {
+        "task_notes": task_notes,
+        "task_description": task_description,
+        "task_completed": task_completed === 0 ? false : true
+    }
+
+
     return transform
 }
-module.exports={
+module.exports = {
     getAll,
     insert
 }
