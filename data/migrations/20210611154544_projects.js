5cf6e6b169af10668d2dc54637833bc81a7c52f9
Daniel Walker <dw765608@gmail.com>
2021-06-11 19:00:03 -0400

    Wip

--- data/migrations/20210611154544_projects.js
+++ data/migrations/20210611154544_projects.js
@@ -24,12 +24,30 @@ exports.up = function(knex) {
   .onUpdate('RESTRICT')
   .onDelete('RESTRICT');
 })
+.createTable("project_resources",table=>{
+  table.increments("project_resources_id")
+  table.integer("resource_id")
+  .unsigned()
+  .notNullable()
+  .references("resource_id")
+  .inTable('resources')
+  .onUpdate('RESTRICT')
+  .onDelete('RESTRICT');
+  table.integer("project_id")
+  .unsigned()
+  .notNullable()
+  .references("project_id")
+  .inTable('projects')
+  .onUpdate('RESTRICT')
+  .onDelete('RESTRICT');
+})
 };
 
 exports.down = function(knex) {
   return knex.schema
-  
+  .dropTableIfExists("project_resources")
   .dropTableIfExists("resources")
   .dropTableIfExists("tasks")
   .dropTableIfExists("projects")
+  
 };
