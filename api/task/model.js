bbd91bba07bd932e41a00677dca39bc52cdddf98
Daniel Walker <dw765608@gmail.com>
2021-06-11 16:18:31 -0400

    all endpoints working

--- api/task/model.js
+++ api/task/model.js
@@ -20,10 +20,29 @@ const getAll=async()=>{
      })
     return revisedData
 }
-const insert=(newTask)=>{
-    const rows = db("tasks")
-    .insert(newTask)
+const findTaskbyId=task_id=>{
+   const rows= db("tasks")
+    .where("task_id",task_id)
     return rows
+}
+
+const insert= async (newTask)=>{
+    const rows = await db("tasks")
+    .insert(newTask)
+    .select("task_notes","task_description","task_completed")
+    const id = await rows[0]
+    const data = await findTaskbyId(id)
+    const {task_notes,task_description,task_completed}=await data[0]
+    console.log(data)
+    //transforms data to boolean
+    const transform =await {
+          "task_notes": task_notes,
+          "task_description": task_description,
+          "task_completed": task_completed===0?false:true
+       }
+  
+    
+    return transform
 }
 module.exports={
     getAll,
