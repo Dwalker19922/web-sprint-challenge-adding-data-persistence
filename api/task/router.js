aea5065dc8989d00f964260ecbca760139f7ce40
Daniel Walker <dw765608@gmail.com>
2021-06-11 14:58:50 -0400

    tasks fetching

--- api/task/router.js
+++ api/task/router.js
@@ -3,11 +3,16 @@
 //setup server
 const express = require('express')
 const tasks = express.Router()
+const tasksM=require("./model")
 
 //test endpoint
-tasks.get('/test',(req, res, next)=>{
-    res.json({message:"connection successful"})
+tasks.get('/',(req, res, next)=>{
+    tasksM.getAll()
+    .then(tsk=>{
+        res.json(tsk)
+    })
     })
 
 //export
+
 module.exports =tasks
